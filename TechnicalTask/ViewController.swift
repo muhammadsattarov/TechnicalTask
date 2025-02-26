f97ecf39f2e2bc9468e3df4b7cff60c24a3b38d8
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2025-02-26 13:15:12 +0500

    Initial Commit

---
+++ TechnicalTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTask
+//
+//  Created by user on 26/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
